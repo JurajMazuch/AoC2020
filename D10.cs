7d5c7710240621108b22203b1bdd559a14c3eaec
JurajMazuch <78723292+JurajMazuch@users.noreply.github.com>
2021-05-17 17:38:43 +0200

    Add files via upload

--- D10.cs
+++ D10.cs
@@ -20,43 +20,37 @@ namespace D10
 			
 			System.Diagnostics.Stopwatch stopWatch = System.Diagnostics.Stopwatch.StartNew();
 			List<int> differences = GetDifferences(input);
-			//PrintInput(differences);
 			int part1 = differences.Where(x => x == 1).Count() * differences.Where(x => x == 3).Count();
 			Console.WriteLine("The number of 1-jolt differences multiplied by the number of 3-jolt differences is {0}", part1);
 			Console.WriteLine("Computation time: {0}\n", stopWatch.Elapsed);
 			
 			stopWatch.Restart();
-			Console.WriteLine("The total number of arrangements that connect the charging outlet to your device is {0}", BruteForce(input, 1) + 1);
+			Dictionary<int, UInt64> cache = new Dictionary<int, UInt64>();
+			Console.WriteLine("The total number of arrangements that connect the charging outlet to your device is {0}", Part2(input, 0, cache));
 			Console.WriteLine("Computation time: {0}\n", stopWatch.Elapsed);
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
 		
-		public static double BruteForce(List<int> input, int startAt)
+		public static UInt64 Part2(List<int> input, int startAt, Dictionary<int, UInt64> cache)
 		{
-			double sum = 0;
-			for (int i = startAt; i < input.Count() - 1; i++)
+			if (startAt == input.Count - 1)
+				return 1;
+			
+			if (cache.ContainsKey(startAt))
+				return cache[startAt];
+			
+			UInt64 pathCount = 0;
+			int i = 1;
+			while (startAt + i < input.Count && input[startAt + i] - input[startAt] <= 3)
 			{
-				int val = input[i];
-				input.RemoveAt(i);
-				if (IsValid(input))
-				{
-					//PrintInput(input);
-					//Console.WriteLine("----------------------");
-					sum += BruteForce(input, i) + 1;
-//					if (sum % 10 == 0) {
-//						Console.WriteLine(sum);
-//					}
-				}
-				input.Insert(i, val);
+				pathCount += Part2(input, startAt + i, cache);
+				i++;
 			}
-			return sum;
-		}
-		
-		public static bool IsValid(List<int> input)
-		{
-			return !GetDifferences(input).Any(x => x > 3);
+			
+			cache.Add(startAt, pathCount);
+			return pathCount;
 		}
 		
 		public static List<int> GetDifferences(List<int> input)
@@ -67,23 +61,6 @@ namespace D10
 				differences.Add(input[i + 1] - input[i]);
 			}
 			return differences;
-		}
-		
-		public static void PrintInput(List<int> input)
-		{
-			List<int> differences = GetDifferences(input);
-			Console.Write("(0), ");
-			for (int i = 1; i < input.Count() - 1; i++)
-			{
-				Console.Write("{0,3}, ", input[i]);
-			}
-			Console.Write("({0})\n", input[input.Count() - 1]);
-			
-			for (int i = 0; i < differences.Count() - 1; i++)
-			{
-				Console.Write("{0,3}, ", differences[i]);
-			}
-			Console.Write("{0, 3}\n", differences[differences.Count() - 1]);
 		}
 	}
 }
